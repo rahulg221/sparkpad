16f5f4aec296ce25a90e1c7f8cce0155d6df5044
rahulg221 <rdg.ghosh@gmail.com>
2025-04-03 02:08:18 -0400

    typo with whitelisting

--- fastapi/main.py
+++ fastapi/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 # Allow requests from http://localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://clusterms.vercel.app/", "http://localhost:3000"],
+    allow_origins=["https://clusterms.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
