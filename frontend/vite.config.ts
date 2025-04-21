604bb77115fc0b8c87a5635527c332afa6a590af
rahulg221 <rdg.ghosh@gmail.com>
2025-04-21 13:43:37 -0400

    clear vite cache

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['chrono-node'],
+    },
+  },
   server: {
     port: 3000,
     open: true
